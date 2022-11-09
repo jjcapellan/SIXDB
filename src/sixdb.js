b981591f3b1dab25acb817a863caeb783518f6a3
jjcapellan <soycape@hotmail.com>
2022-11-09 13:08:54 +0100

    Fix: Sixdb.newStore not working with null callback parammeters

--- src/sixdb.js
+++ src/sixdb.js
@@ -495,7 +495,7 @@ Sixdb.prototype.checkStore = function (storeName, callback = voidFn) {
  */
 Sixdb.prototype.newStore = function (
   storeName,
-  { keyPath, autoIncrement, successCallback, errorCallback } = {}
+  { keyPath, autoIncrement, successCallback=voidFn, errorCallback=voidFn } = {}
 ) {
   let args = [
     storeName,
